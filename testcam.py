007b4f7ce637030a2fbe7a338541f1e1fc3b695e
Bob McHenry <bobmchenryvideo@gmail.com>
2017-03-25 21:59:08 -0700

    Test Commit

--- testcam.py
+++ testcam.py
@@ -4,7 +4,7 @@ import picamera
 # X is number of photos, t is time interval
 def timelapse(x, t):
     with picamera.PiCamera() as camera:
-
+	# Test comment
         for i in range(1,x+1):
             camera.start_preview()
             time.sleep(t)
